41936c292d8422ff011d2179c1f0644c747056d3
doraig <gopiraj.dorairaj@gmail.com>
2018-11-20 11:26:12 -0500

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/FirstProg.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FirstProg.dir/main.cpp.obj"
+  "FirstProg.pdb"
+  "FirstProg.exe"
+  "FirstProg.exe.manifest"
+  "libFirstProg.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FirstProg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
